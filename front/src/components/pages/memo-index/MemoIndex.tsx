bc9bf610a513fd28819bcd06840b4447e3c1babb
mittsutaka <mitsutaka.murao@tryeting.jp>
2022-07-20 08:59:27 +0900

    ちょっと修正

--- front/src/components/pages/memo-index/MemoIndex.tsx
+++ front/src/components/pages/memo-index/MemoIndex.tsx
@@ -13,7 +13,7 @@ const MemoIndex: React.FC<Props> = ({ memos }) => {
     <Layout>
       <div className="flex flex-wrap">
         {memos.map((memo) => {
-          return <MemoCard className="mr-4 h-56 w-48" memo={memo} />;
+          return <MemoCard key={memo.Id} className="mr-4 h-56 w-48" memo={memo} />;
         })}
       </div>
     </Layout>
