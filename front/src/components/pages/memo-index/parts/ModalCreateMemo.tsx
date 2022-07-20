6a7571b253d378813a3d7b298b4fd9c223a7b998
mittsutaka <mitsutaka.murao@tryeting.jp>
2022-07-20 13:09:01 +0900

    カテゴリー追加

--- front/src/components/pages/memo-index/parts/ModalCreateMemo.tsx
+++ front/src/components/pages/memo-index/parts/ModalCreateMemo.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Label, Modal, Textarea, TextInput } from "flowbite-react";
+import { Label, Modal, Select, Textarea, TextInput } from "flowbite-react";
 import Button from "../../../ui-elements/Button";
+import { Category } from "../../../../models/category/type";
+import useFetch from "../../../../hooks/useFecth";
 
 type Props = {
   className?: string;
@@ -9,7 +11,10 @@ type Props = {
   showModal: boolean;
 };
 
+const categoriesEndPoint = `${process.env.REACT_APP_BACKEND_END_POINT}/categories`;
+
 const ModalCreateMemo: React.FC<Props> = ({ className, title, showModal, switchCreateModal }) => {
+  const { data, error } = useFetch<Category[]>(categoriesEndPoint);
   return (
     <Modal show={showModal} onClose={() => switchCreateModal(false)}>
       <Modal.Header>{title}</Modal.Header>
@@ -19,9 +24,19 @@ const ModalCreateMemo: React.FC<Props> = ({ className, title, showModal, switchC
             <div className="mb-2 block">
               <Label htmlFor="contents" value="タイトル" />
             </div>
-            <TextInput type="text" id="title" placeholder="" required={true} />
+            <TextInput type="text" placeholder="" required={true} />
           </div>
-          <div id="textarea">
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="countries" value="Select your country" />
+            </div>
+            <Select required={true}>
+              {data?.map((category) => {
+                return <option key={category.Id}>{category.Name}</option>;
+              })}
+            </Select>
+          </div>
+          <div>
             <div className="mb-2 block">
               <Label htmlFor="contents" value="内容" />
             </div>
